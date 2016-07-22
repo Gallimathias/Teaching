363526006fd9e032862098372ab82064270c2f23
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2016-07-22 21:45:01 +0200

    Noob Dev Teaching gedöns Version 2 (22.07.2016)
    Topics:
    
    - Loops
    - Console Output
    - Console Input
    - Methods
    
    Add "Grapics" to the Game

--- Hanoi/Hanoi/Program.cs
+++ Hanoi/Hanoi/Program.cs
@@ -8,89 +8,91 @@ namespace Hanoi
 {
     class Program
     {
+        static int diskCount = 7;
+
         static void Main(string[] args)
         {
-            int diskCount = 14;
-
             int[] stickA = new int[diskCount];
             int[] stickB = new int[diskCount];
             int[] stickC = new int[diskCount];
 
             //stickC[7] = 3;
-
             int loops = 0;
+
             while (loops < diskCount)
             {
-                
-                    stickA[loops] = loops + 1;
 
+                if(diskCount - loops != 3 && diskCount - loops != 4)
+                {
+                    Gravity(stickA, diskCount - loops);
+                }
                 loops++;
             }
-
-            int maxspaces = diskCount - 1;
-            
-            for (int loopcount = 0; loopcount < diskCount; loopcount++)
+            DrawSticks(stickA, stickB, stickC);
+            while (true) // GamingLoop
             {
 
-                Console.ForegroundColor = (ConsoleColor)stickA[loopcount];
-                for (int i = 0; i < maxspaces; i++)
+                ConsoleKeyInfo s = Console.ReadKey();
+                Console.Clear();
+                DrawSticks(stickA, stickB, stickC);
+                Console.ForegroundColor = ConsoleColor.White;
+                if (s.Key == ConsoleKey.D1)
                 {
-                    Console.Write(" ");
+                    Console.WriteLine("You have selected the first tower");
                 }
+                else if(s.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("You have selected the second tower");
+                }
+                else if (s.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine("You have selected the third tower");
+                }
+            }
+        }
 
-                if (0 < stickA[loopcount])
-                {
-                    for (int i = 0; i < stickA[loopcount]; i++)
-                    {
-                        Console.Write("##");
-                    }
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    for (int i = 0; i < loopcount +1; i++)
-                    {
-                        Console.Write("  ");
-                    }
-                }
-
-                
-                for (int i = 0; i < maxspaces + 1; i++)
-                {
-                    Console.Write(" ");
-                }
-
-                for (int i = 0; i < maxspaces; i++)
-                {
-                    Console.Write(" ");
-                }
-                for (int i = 0; i < stickB[loopcount]; i++)
-                {
-                    Console.Write("##");
-                }
-                for (int i = 0; i < maxspaces; i++)
-                {
-                    Console.Write(" ");
-                }
-
-                for (int i = 0; i < maxspaces; i++)
-                {
-                    Console.Write(" ");
-                }
-                for (int i = 0; i < stickC[loopcount]; i++)
-                {
-                    Console.Write("##");
-                }
-                for (int i = 0; i < maxspaces; i++)
-                {
-                    Console.Write(" ");
-                }
-                maxspaces--;
-                
+        static void DrawSticks(int[] stickA, int[] stickB, int[] stickC)
+        {
+            for (int i = 0; i < diskCount; i++)
+            {
+                DrawLine(stickA[i]);
+                DrawLine(stickB[i]);
+                DrawLine(stickC[i]);
                 Console.WriteLine();
             }
+        }
 
-            Console.ReadKey();
+        static void DrawLine(int disc)
+        {
+            int maxspaces = diskCount - 1;
+            int leftspacefromdisk = diskCount - disc;
+            Console.CursorLeft += leftspacefromdisk + 2;
+
+            Console.ForegroundColor = (ConsoleColor)disc;
+            for (int i = 0; i < disc; i++)
+            {
+                Console.Write("##");
+            }
+            Console.CursorLeft += leftspacefromdisk;
+        }
+
+        static void Gravity(int[] stick, int disc)
+        {
+            if (stick[stick.Length-1] == 0)
+            {
+                stick[stick.Length-1] = disc;
+            }
+            else
+            {
+                for (int i = 0; i < stick.Length; i++)
+                {
+                    if (stick[i] > 0)
+                    {
+                        stick[i - 1] = disc;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
